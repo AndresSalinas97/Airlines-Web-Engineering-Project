13da9b9f8ef7db5fc01450d9b0dedb1c289d4aff
Stefan <stefan_code@yahoo.com>
2019-03-29 04:20:43 +0100

    Very late

--- APP/scripts/airport.js
+++ APP/scripts/airport.js
@@ -12,8 +12,8 @@ const vm = new Vue({
       })
       axios.get("https://airports-api.s3-us-west-2.amazonaws.com/iata/"+window.location.pathname.substring(10).toLowerCase()+".json")
 		.then(response => {
-			console.log(window.location.pathname.substring(10).toLowerCase());
-			console.log(response);
+			//console.log(window.location.pathname.substring(10).toLowerCase());
+			//console.log(response);
 			this.results.googlemaps = 'http://google.com/maps?q=' + response.data.latitude + ',' + response.data.longitude;
 			this.results.timezone = 'UTC' + response.data.utc_offset;
 			console.log(this.results.googlemaps);
@@ -23,4 +23,5 @@ const vm = new Vue({
 		console.log(error.response);
 		})
    }
+
 });
--- APP/scripts/carrier.js
+++ APP/scripts/carrier.js
@@ -1,7 +1,8 @@
 const vm = new Vue({
     el: '#app',
     data: {
-      results:[],
+      results:'',
+      things:[]
     },
     mounted() {
       axios.get("http://localhost:8080"+window.location.pathname)
@@ -10,5 +11,14 @@ const vm = new Vue({
       .catch(error =>{
         console.log(error.response)
       })
+      axios.get("http://localhost:8080"+window.location.pathname.substring(0,12)+"/user-ratings")
+      .then(response => {this.things = response.data.data;
+      console.log(this.things);
+
+	  })
+      .catch(error =>{
+        console.log(error.response)
+      })
+    
    }
 });
